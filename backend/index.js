8fad39fb102873b1af601e55475f2fa92880a6bd
Pratika NG <pratika.05ng@gmail.com>
2025-11-14 15:47:37 +0530

    routes setup for posts, users and comments

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+dotenv.config()
+import express from "express"
+import dotenv from "dotenv"
+import userRouter from "./routes/user.route.js"
+import postsRouter from "./routes/post.route.js"
+import commentsRouter from "./routes/comment.route.js"
+
+const app = express()
+const port = process.env.PORT || 3001;
+
+app.get("/test",(req,res)=>{
+  res.status(200).send("it works!")
+})
+
+app.use("/users",userRouter)
+app.use("/posts",postsRouter)
+app.use("/comments",commentsRouter)
+
+app.listen(port,()=>{
+    console.log(`App is listening to ${port}`)
+})
+
