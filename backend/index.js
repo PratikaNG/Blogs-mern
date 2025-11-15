ee9a22183554169ad0f4dd734410fed3d289f5fe
Pratika NG <pratika.05ng@gmail.com>
2025-11-15 19:13:53 +0530

    posts crud and error handling

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ import connectDB from "./lib/connectDB.js"
 const app = express()
 const port = process.env.PORT || 3001;
 connectDB()
-
+app.use(express.json())
 app.get("/test",(req,res)=>{
   res.status(200).send("it works!")
 })
@@ -17,6 +17,16 @@ app.get("/test",(req,res)=>{
 app.use("/users",userRouter)
 app.use("/posts",postsRouter)
 app.use("/comments",commentsRouter)
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+
+  res.json({
+    message: error.message || "Something went wrong!",
+    status: error.status,
+    stack: error.stack,
+  });
+});
 
 app.listen(port,()=>{
     console.log(`App is listening to ${port}`)
