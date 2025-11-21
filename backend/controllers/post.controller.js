a30844f6ace38f36fbe6b227dfa29f3572d701b0
Pratika NG <pratika.05ng@gmail.com>
2025-11-21 17:33:00 +0530

    infinte scroll with react query

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -9,8 +9,15 @@ const imagekit = new ImageKit({
   urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
 });
 export const getPosts = async(req,res) =>{
-      const posts = await Post.find()
-    res.status(200).send(posts) 
+  const page = parseInt(req.query.page || 1)
+  const limit = parseInt(req.query.limit || 5)
+  const posts = await Post.find()
+  .populate("user","username")
+  .limit(limit)
+  .skip((page-1)*limit)
+  const totalPosts = await Post.countDocuments()
+  const hasMore=  page * limit < totalPosts
+  res.status(200).json({posts,hasMore}) 
 
 }
 export const getPost = async(req,res) =>{
--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -1,26 +1,53 @@
 import React from 'react'
 import PostListItem from './PostListItem'
-import { useQuery } from '@tanstack/react-query'
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query'
 import axios from "axios"
+import InfiniteScroll from "react-infinite-scroll-component"
 
-
-const PostList = () => {
+const PostList = (pageParam) => {
   const fetchPosts = async()=>{
-  const res = await axios.get(`${import.meta.env.VITE_API_URL}/posts`)
+  const res = await axios.get(`${import.meta.env.VITE_API_URL}/posts`,{
+    params:{page:pageParam}
+  })
   return res.data
 }
-  const { isPending, error, data } = useQuery({
-    queryKey: ['repoData'],
-    queryFn: () =>fetchPosts(),
+   const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery({
+    queryKey: ['posts'],
+    queryFn: (pageParam = 1)=>fetchPosts(pageParam),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, pages) => lastPage.hasMore ? pages.length + 1 : undefined,
   })
 
-  if (isPending) return 'Loading...'
-
+  console.log("data",data)
+  if (status === "loading") return 'Loading...'
+  
   if (error) return 'An error has occurred: ' + error.message
+  const allPosts = data.pages.flatMap((page)=>page.posts) || []
   console.log("data",data)
   return (
     <div className='flex flex-col gap-4 mb-8'>
-      <PostListItem/>
+      <InfiniteScroll
+  dataLength={allPosts.length} //This is important field to render the next data
+  next={fetchNextPage}
+  hasMore={!hasNextPage}
+  loader={<h4>Loading more posts...</h4>}
+  endMessage={
+    <p>
+      <b>All posts loaded!</b>
+    </p>
+  }
+>
+
+      {allPosts.map((item)=><PostListItem key={item._id} post={item}/>)}</InfiniteScroll>
+      
       <PostListItem/>
       <PostListItem/>
       <PostListItem/>
