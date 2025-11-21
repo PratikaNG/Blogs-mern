df46829e6cb76a7084a9477f217ef93357e30317
Pratika NG <pratika.05ng@gmail.com>
2025-11-21 18:14:01 +0530

    Add new comment with mutations

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import IkImage from './IkImage'
+import {format} from "timeago.js"
 
-const Comment = () => {
+const Comment = ({comment}) => {
   return (
     <div className='p-4 bg-slate-50 rounded-xl mb-8'>
         <div className='flex items-center gap-4'>
             <IkImage src="userImg.jpeg" className="w-10 h-10 rounded-full object-cover" w="40"/>
+            {/* {comment.user.img && <IkImage src={comment.user.img} className="w-10 h-10 rounded-full object-cover" w="40"/>} */}
             <span className='font-medium'>John Doe</span>
+            <span className='font-medium'>{comment.user.username}</span>
             <span className="text-sm text-gray-500">2 days ago</span>
+        {/* <span>{format(post.createdAt)}</span> */}
+
         </div>
         <div className='mt-4'>
+          {/* {comment.desc} */}
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam hic debitis expedita unde minus ex, ipsum voluptatibus illum quae temporibus officia beatae nulla! Provident dolor nostrum quidem id earum veniam?</p>    
          </div>
       
--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Comment from './Comment'
 import axios from 'axios'
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { toast } from 'react-toastify'
 
 const fetchComments = async(postId) =>{
     const slug = useParams()
@@ -10,7 +11,8 @@ const fetchComments = async(postId) =>{
   }
 
 const Comments = () => {
-  
+  const { getToken } = useAuth();
+  const { isLoaded, isSignedIn } = useUser();
     const {isPending,error,data} = useQuery({
       queryKey:["comments",postId],
       queryFn:()=>fetchComments(postId)
@@ -18,15 +20,48 @@ const Comments = () => {
     if (isPending) return 'Loading...'
     
     if (error) return 'An error has occurred: ' + error.message
+
+    const queryClient = useQueryClient()
+
+    const mutation = useMutation({
+    mutationFn: async (newComment) => {
+      console.log("newComment", newComment);
+      const token = await getToken();
+      console.log("token", token);
+
+      return axios.post(`${import.meta.env.VITE_API_URL}/comments/${postId}`, newComment, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey:["comments",postId]})
+    },
+    onError:(err)=>{
+      toast.error(err.response.data)
+    }
+  });
+
+   const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target)
+    const data = {
+      desc:formData.get("desc")
+    }
+
+    mutation.mutate(data)
+  }
+
   return (
-    <div className='flex flex-col gap-8 lg:w-3/5'>
+    <div className='flex flex-col gap-8 lg:w-3/5 mb-12'>
       <h1 className='underline text-xl text-gray-500'>Comments</h1>
-      <div className='flex items-center justify-between gap-8 w-full'>
-        <textare placeholder="Write a comment..." className="w-full p-4 rounded-xl"/>
-        <button className="bg-blue-800 px-4 py-3 text-white font-medium rounded-xl">
+      <form onSubmit={handleSubmit} className='flex items-center justify-between gap-8 w-full'>
+        <textare name='desc' placeholder="Write a comment..." className="w-full p-4 rounded-xl"/>
+        <button type='submit' className="bg-blue-800 px-4 py-3 text-white font-medium rounded-xl">
           Send
         </button>
-      </div>
+      </form>
       {data.map((item)=><Comment key={item._id} comment={item}/>)}
       <Comment/>
       <Comment/>
